0cc26d9dd3755f54001a6bce632a0f1c584304cf
ajberasategui <ajberasategui@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-20 21:33:28 +0000

    Modified to default search under top parent categories
    git-svn-id: https://plugins.svn.wordpress.org/ab-categories-search-widget/trunk@807788 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ab-category-search.php
+++ ab-category-search.php
@@ -61,7 +61,8 @@ class ABCategorySearch  extends WP_Widget {
 				<?php foreach( $instance['categories'] as $name => $cat_id ) : ?>
 					<label for="<?php echo $this->id; ?>_cat_sel"><?php echo $name; ?></label>
 					<select name="search_cat[]" id="<?php echo $this->id; ?>_cat_sel">
-						<option value="0" <?php selected( true, in_array(0, $incoming_search_cats ) ); ?>>
+						<option value="<?php echo $cat_id; ?>" 
+							<?php selected( true, in_array($cat_id, $incoming_search_cats ) ); ?>>
 							<?php _e( 'All', 'absc' ); ?>
 						</option>
 						<?php
